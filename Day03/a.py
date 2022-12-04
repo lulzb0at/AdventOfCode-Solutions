7b330205ad2a08f95f79f0d385cca38005e642e6
Anonymous <119621366+lulzb0at@users.noreply.github.com>
2022-12-03 21:38:44 -0500

    Update a.py

--- Day03/a.py
+++ Day03/a.py
@@ -1,8 +1,9 @@
-inpout = '' # path to input file
-with open(input,'r') as fh: lines = fh.read()
+with open('input.txt','r') as fh: lines = fh.read()
    
 total = []
+
 for line in lines.splitlines():  
     letter = ( set(line[:int( len(line) /2 ) ]) & set(line[int( len(line) /2 ) :])).pop()
     total.append(ord(letter)-96) if letter.islower() else total.append(ord(letter)-38)
+   
 print(sum(total))
