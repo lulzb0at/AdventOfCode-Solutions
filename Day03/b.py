da90290cb123e54dc26a3d9fabbf3e87d0aab4a3
Anonymous <119621366+lulzb0at@users.noreply.github.com>
2022-12-03 21:39:07 -0500

    Update b.py

--- Day03/b.py
+++ Day03/b.py
@@ -1,8 +1,9 @@
-input = "" # path to your inpout file
-with open(input,'r') as fh: lines = fh.read()
+with open('input.txt','r') as fh: lines = fh.read()
     
 total = []
+
 for a,b,c in zip(*[iter(lines.splitlines())]*3):   
     letter = (set(a.strip()) & set(b.strip()) & set(c.strip())).pop()
     total.append(ord(letter)-96) if letter.islower() else total.append(ord(letter)-38)
+    
 print(sum(total))
