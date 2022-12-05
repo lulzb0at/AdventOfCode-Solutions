f12e3eb1e2c998ad8f12fdcb183f7844af29f4fc
Anonymous <119621366+lulzb0at@users.noreply.github.com>
2022-12-05 01:32:12 -0500

    Create a.py
    hand jam stacks input; solution is top-of-stack, for each stack. 
    todo: display solution string

---
+++ Day05/a.py
@@ -0,0 +1,21 @@
+stacks = {
+    '1' : ['C', 'Z', 'N', 'B', 'M', 'W', 'Q', 'V'],
+    '2' : ['H', 'Z', 'R', 'W', 'C', 'B'],
+    '3' : ['F', 'Q', 'R', 'J'],
+    '4' : ['Z', 'S', 'W', 'H', 'F', 'N', 'M', 'T'],
+    '5' : ['G', 'F', 'W', 'L', 'N', 'Q', 'P'],
+    '6' : ['L', 'P', 'W'],
+    '7' : ['V', 'B', 'D', 'R', 'G', 'C', 'Q', 'J'],
+    '8' : ['Z', 'Q', 'N', 'B', 'W'],
+    '9' : ['H', 'L', 'F', 'C', 'G', 'T', 'J'],
+    }
+
+for line in lines.splitlines()[10:]:   
+    stack = int(line.split()[1])
+    src = line.split()[3]
+    dst = line.split()[5]
+    
+    for _ in range(stack):
+        stacks[dst].append(stacks[src].pop())
+
+print(stacks)
