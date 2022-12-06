ee8eb8cd983c0027ca62beaa849c9e5803e78d3f
Anonymous <119621366+lulzb0at@users.noreply.github.com>
2022-12-06 01:25:23 -0500

    Update a.py

--- Day06/a.py
+++ Day06/a.py
@@ -1,7 +1,6 @@
 with open('input.txt','r') as fh: line = fh.read()
 
 for i in range(len(line)):   
-    print('%d: %s' % (i, line[i])) #
     if len(set(line[i:i+4])) == 4:
         print(i+4)
         break
